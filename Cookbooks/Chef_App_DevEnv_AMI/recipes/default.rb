f08567948d000111e6629689d9544a0c08b596e6
rramesh16 <57910296+rramesh16@users.noreply.github.com>
2020-01-27 15:27:30 +0000

    Update default.rb

--- Cookbooks/Chef_App_DevEnv_AMI/recipes/default.rb
+++ Cookbooks/Chef_App_DevEnv_AMI/recipes/default.rb
@@ -8,7 +8,7 @@ apt_update "update" do
   action :update
 end
 
-package "python3-dev" do
+package "python3" do
   action :install
 end
 
@@ -16,17 +16,35 @@ package "python3-pip" do
   action :install
 end
 
-# execute "pip3 install requirements" do
-#   command "pip3 install -r /home/ubuntu/app/requirements.txt"
-#   cwd '/home/ubuntu'
-# end
-# 
-# directory '/home/vagrant/Downloads' do
-#   action :create
-#   mode '0777'
-# end
-#
-# file '/home/vagrant/Downloads/ItJobsWatchTop30.csv' do
-#   action :create
-#   mode '0777'
-# end
+
+directory '/home/ubuntu/app/' do
+  action :create
+  mode '0777'
+end
+
+template '/home/ubuntu/app/requirements.txt' do
+  source 'requirements.txt.erb'
+  mode '0777'
+end
+
+execute "install requirements" do
+  command "sudo -H pip3 install -r /home/ubuntu/app/requirements.txt"
+end
+
+execute "install pytest" do
+  command "pip3 install pytest"
+end
+
+execute 'java' do
+  command 'sudo apt-get install default-jre -y'
+  action :run
+end
+
+file '/home/ubuntu/app/requirements.txt' do
+  action :delete
+end
+
+directory '/home/ubuntu/app/' do
+  action :delete
+end
+
